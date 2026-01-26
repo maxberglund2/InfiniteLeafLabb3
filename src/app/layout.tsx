d63bcbd325c41de396c54f32deebf0941429a05a
Greed <106820018+maxberglund2@users.noreply.github.com>
2026-01-26 21:41:33 +0100

    Refactor layout and update pointer events for menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,23 +27,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="relative z-99999">
-          <AuthProvider>
-            <Navbar />
-            <main className="z-99999">{children}</main>
-            <Footer />
-          </AuthProvider>
-        </div>
-
-        <div className="fixed inset-0 w-full h-full pointer-events-none z-0">
+        {/* Background */}
+        <div className="fixed inset-0 -z-10 pointer-events-none">
           <Iridescence
             color={[0, 0.2, 0.1]}
             mouseReact
             amplitude={0.1}
             speed={0.2}
           />
-          <button className="text-blue-500 bg-red-700">hello</button>
         </div>
+
+        {/* App */}
+        <AuthProvider>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
